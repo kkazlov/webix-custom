2b235ff5003a529d2fe439e3509b63fc58592048
kkazlov <kkazlov.it@gmail.com>
2021-09-30 11:45:59 +0300

    less1 - task1

--- webix/app.js
+++ webix/app.js
@@ -1,6 +1,36 @@
+import task1 from "./components/task1.js";
+
+webix.protoUI(
+    {
+        name: "mybutton",
+        $cssName: "button",
+        $init: function (config) {
+            const state = config.value || 0;
+            config.value = config.states[state];
+            webix.html.addCss(this.$view, `webix_state_${state}`);
+
+            this.attachEvent("onItemClick", () => {
+                let state = this.config.state;
+                webix.html.removeCss(this.$view, `webix_state_${state}`);
+
+                state++;
+                if (state > 2) {
+                    state = 0;
+                }
+                this.config.state = state;
+                this.config.label = this.config.states[state];
+                this.refresh();
+
+                webix.html.addCss(this.$view, `webix_state_${state}`);
+                this.callEvent("onStateChange", [state]);
+            });
+        },
+    },
+    webix.ui.button
+);
+
 webix.ready(() => {
     webix.ui({
-        
+        rows: [task1],
     });
-
 });
